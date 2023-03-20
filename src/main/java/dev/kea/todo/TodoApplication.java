fb9ab6ed9965f0cc2ea16823e4d9d6eeebfb0238
Lars Kruse <lakruzz@inc-inc.dk>
2023-03-20 15:03:02 +0000

    Added boiler plate code from start.spring.io close #2

---
+++ src/main/java/dev/kea/todo/TodoApplication.java
@@ -0,0 +1,13 @@
+package dev.kea.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApplication.class, args);
+	}
+
+}
